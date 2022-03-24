524181adf2c7a551d2b6f45f84fb008ecfff12cd
Asier Pabolleta Martorell <asier.pabolleta@unavarra.es>
2022-03-24 16:35:25 +0100

    [NEW] Create pages

---
+++ src/pages/about/AboutUs.js
@@ -0,0 +1,11 @@
+import './AboutUs.css';
+
+function AboutUs() {
+    return (
+        <div>
+            <h2>About Us</h2>
+        </div>
+    )
+}
+
+export default AboutUs;
---
+++ src/pages/contact/Contact.js
@@ -0,0 +1,11 @@
+import './Contact.css';
+
+function Contact() {
+    return (
+        <div>
+            <h2>Contact Us</h2>
+        </div>
+    )
+}
+
+export default Contact;
