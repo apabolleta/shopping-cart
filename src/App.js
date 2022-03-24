78333bdf211f745cf57f5a7d21ffc492a36ce43e
Asier Pabolleta Martorell <asier.pabolleta@unavarra.es>
2022-03-24 16:49:17 +0100

    [UPDATE] Update project dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Header from './layouts/header/Header';
 import Footer from './layouts/footer/Footer';
+import { Routes, Route } from 'react-router-dom';
+import AboutUs from 'pages/about/AboutUs';
+import Contact from 'pages/contact/Contact';
 
 function App() {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
     </div>
   );
