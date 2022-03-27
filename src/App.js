a48431da965cbef071ae53d10df5212327cb1dbd
Asier Pabolleta Martorell <asier.pabolleta@unavarra.es>
2022-03-27 22:35:18 +0200

    [EDIT] Edit BrowserRouter component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import './App.css';
 import Header from 'layouts/header/Header';
 import Footer from 'layouts/footer/Footer';
 import AppRoutes from 'pages/Routes';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Header />
-        <AppRoutes />
-        <Footer />
-      </div>
-    </Router>
+    <div className="app">
+      <Header />
+      <AppRoutes />
+      <Footer />
+    </div>
   );
 }
 
