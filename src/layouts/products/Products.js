44e49d352fd6a7bd20ff5f0fa9a4e689b01e6480
Asier Pabolleta Martorell <asier.pabolleta@unavarra.es>
2022-03-30 22:04:00 +0200

    [EDIT] Set product array keys

--- src/layouts/products/Products.js
+++ src/layouts/products/Products.js
@@ -7,7 +7,7 @@ function Products(props) {
             <div className="container my-3">
                 <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-5 gy-3 g-1">
                     {props.products.map((p) => (
-                        <div className="col">
+                        <div className="col" key={p.id}>
                             <Product
                                 id={p.id}
                                 name={p.name}
