5d077cbbaa2b7a0417e66a1cee488a41c0292538
Asier Pabolleta Martorell <asier.pabolleta@unavarra.es>
2022-04-05 16:53:56 +0200

    [EDIT] Store header style edit

--- src/layouts/storeHeader/StoreHeader.js
+++ src/layouts/storeHeader/StoreHeader.js
@@ -6,13 +6,13 @@ function StoreHeader(props) {
     return (
         <div className="container py-2 mt-3">
             <div className="row">
-                <div className="col-3 d-flex">
+                <div className="col-3 d-flex justify-content-end">
+                    <CurrencySelector />
                 </div>
                 <div className="col-6">
                     <Search />
                 </div>
-                <div className="col-3 d-flex justify-content-between">
-                    <CurrencySelector />
+                <div className="col-3 d-flex justify-content-start">
                     <Cart
                         cart={props.cart}
                         onClickAdd={props.onClickAdd}
