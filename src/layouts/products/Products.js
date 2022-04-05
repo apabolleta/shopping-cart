73119591e87a5ba2c967fd4845e2ccf419facfaf
Asier Pabolleta Martorell <asier.pabolleta@unavarra.es>
2022-04-05 16:59:43 +0200

    [EDIT] Products layout grid edit

--- src/layouts/products/Products.js
+++ src/layouts/products/Products.js
@@ -5,7 +5,7 @@ function Products(props) {
     return (
         <div className="products">
             <div className="container my-3">
-                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-5 gy-3 g-1">
+                <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xxl-5 gy-3 g-1">
                     {props.products.map((p) => (
                         <div className="col" key={p.id}>
                             <Product
